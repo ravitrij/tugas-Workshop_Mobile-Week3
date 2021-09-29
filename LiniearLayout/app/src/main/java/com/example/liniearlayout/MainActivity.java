5a191e98c7dbf402a6328b8ec9fdb1a7e8a01235
ravitrij <ravijulianto46@gmail.com>
2021-09-29 21:18:18 +0700

    Tugas-Workshop_Mobile-Week3

---
+++ LiniearLayout/app/src/main/java/com/example/liniearlayout/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.liniearlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    EditText to,  subject, Message;
+    Button send;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        to = findViewById(R.id.to);
+        subject = findViewById(R.id.subject);
+        Message = findViewById(R.id.message);
+        send = findViewById(R.id.send);
+    }
+}
---
+++ RelativeLayout/app/src/main/java/com/example/relativelayout/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.relativelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity{
+    DatePickerDialog picker;
+    EditText eText;
+    Button btnGet;
+    TextView tvw;
+    @Override
+    protected void onCreate(Bundle savedInstanceStates) {
+        super.onCreate(savedInstanceStates);
+        setContentView(R.layout.activity_main);
+        tvw =(TextView) findViewById(R.id.textView1);
+        eText=(EditText) findViewById(R.id.editText1);
+        eText.setInputType(InputType.TYPE_NULL);
+        eText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                picker = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int monthOfYear,
+                                          int dayOfMonth) {
+                       eText.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                    }
+                }, year, month, day);
+                picker.show();
+            }
+        });
+        btnGet=(Button) findViewById(R.id.button1);
+        btnGet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvw.setText("Selected Date :" + eText.getText());
+            }
+        });
+    }
+}
